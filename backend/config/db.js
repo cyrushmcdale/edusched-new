fd3eb95c9a5e69d9b13d2abe26b232ab9f17405b
Cyrush Mcdale Buenafe <cyrushmcdale5@gmail.com>
2025-12-02 17:57:21 +0800

    Fix: DB Reconnect

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,26 @@
 const mysql = require("mysql2");
 require("dotenv").config();
+const url = require("url");
+
+// Parse Clever Cloud MySQL URI
+const dbUrl = url.parse(process.env.MYSQL_ADDON_URI);
+
+const [user, password] = dbUrl.auth.split(":");
 
 const db = mysql.createPool({
-  uri: process.env.MYSQL_ADDON_URI,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  host: dbUrl.hostname,
+  user,
+  password,
+  database: dbUrl.pathname.replace("/", ""),
+  port: dbUrl.port,
+  ssl: { rejectUnauthorized: false },
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 db.getConnection((err, con) => {
   if (err) {
-    console.log("❌ DB not connected:", err);
+    console.log("❌ DB not connected:", err.code, err.sqlMessage || "");
   } else {
     console.log("🟢 Database connected");
     con.release();
